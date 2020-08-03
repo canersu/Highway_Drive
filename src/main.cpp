da5ecea7f77f03c4afb7b560278d1a1e5c92356d
canersu <canersu34@gmail.com>
2020-08-03 04:16:35 +0300

    remove comments

--- src/main.cpp
+++ src/main.cpp
@@ -229,15 +229,6 @@ int main() {
 
           json msgJson;
 
-          // vector<double> next_x_vals;
-          // vector<double> next_y_vals;
-
-          /**
-           * TODO: define a path made up of (x,y) points that the car will visit
-           *   sequentially every .02 seconds
-           */
-
-
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
 
